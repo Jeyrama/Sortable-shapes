b95b220c7b93c1617a44848a1a52141292062ce0
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-27 13:51:47 -0700

    Update Sortable-shapes.js

--- Sortable-shapes.js
+++ Sortable-shapes.js
@@ -30,3 +30,13 @@ Task:
 
 
 // Solution
+
+class Shape {
+  constructor(area) {this.area = area}
+  valueOf() {return Number(this.area)}
+}
+class Square extends Shape{constructor(side) {super(side * side)}}
+class Rectangle extends Shape{constructor(height, width) {super(height * width)}}
+class Triangle extends Shape{constructor(base, height) {super((base * height) / 2)}}
+class Circle extends Shape{constructor(radius) {super(radius * radius * Math.PI)}}
+class CustomShape extends Shape{constructor(area) {super(area)}}
